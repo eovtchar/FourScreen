b866d4728fbd2cdbd668dc4cdea07ee4e9f2229e
eovtchar <eovtchar@mail.ru>
2015-04-13 23:03:00 +0300

    Version 1

--- app/src/main/java/ee/eovchar/fourscreen/FourSCR.java
+++ app/src/main/java/ee/eovchar/fourscreen/FourSCR.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class FourSCR extends ActionBarActivity {
@@ -15,8 +16,14 @@ public class FourSCR extends ActionBarActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four_scr);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(ThirdSCR.EXTRA_MESSAGE3);
+                //SENDING THE MESSAGE
+        TextView editText = (TextView) findViewById(R.id.textViewXML);
+        editText.setText(message);
     }
 }
